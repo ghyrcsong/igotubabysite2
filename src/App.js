47800f5d834d7690cd5e721ad5a27ba0aafc61fb
ghyrcsong <yrcsong@gmail.com>
2023-06-16 15:51:19 -0700

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from './pages/Home';
+import Directory from './pages/Directory';
+import About from './pages/About';
+import Contact from './pages/Contact'; 
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className='App'>
+      <Navbar dark color='pink' sticky='top' expand='md'>
+        <Container>
+          <h1>IGotYouBaby</h1>
+          <h3>An Essential For Families With Baby</h3>
+        </Container>
+      </Navbar>
+        <nav>
+          <Link to='/'>Home</Link> | <Link to='/directory'>Directory</Link> | <Link to='/about'>About</Link> | <Link to='/contact'>Contact</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/directory' element={<Directory />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
     </div>
   );
 }
---
+++ src/pages/Directory.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Directory = () => {
+    return <h1>Directory Page</h1>;
+}
+
+export default Directory;
